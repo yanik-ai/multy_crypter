536ef4685e845364614ed841d19ca759b5ab4c5c
Yanik <yanikkoval@devrecords.com>
2016-03-09 23:14:26 +0200

    improve morse code code ;)

--- algorithms/morse_code.py
+++ algorithms/morse_code.py
@@ -6,40 +6,39 @@ from string import ascii_letters, digits, whitespace
 class MorseCode(object):
 
     def __init__(self):
-        # init alphabet and reversed alphabet
-        self.alphabet = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..',
-                         'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
-                         'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
-                         'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
-                         'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-                         'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
-                         'Y': '-.--', 'Z': '--..', ' ': '−···−', '0': '-----',
-                         '1': '.----',  '2': '..---', '3': '...--', '4': '....-',
-                         '5': '.....', '6': '-....',  '7': '--...',  '8': '---..',
-                         '9': '----.'
-                         }
+        # International Morse Code Alphabet
+        # http://morsecode.scphillips.com/morse2.html
+        self.alphabet = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+                         'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+                         'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+                         'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+                         'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+                         'Z': '--..', '0': '-----', '1': '.----', '2': '..---',
+                         '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+                         '7': '--...', '8': '---..', '9': '----.', ',': '--..--',
+                         ' ': '/'}
         self.inveresed_alphabet = dict((v, k) for (k, v) in self.alphabet.items())
         # we will encode only allowed characters
-        self.allowed_characters = ascii_letters + digits + whitespace
+        self.allowed_characters = ''.join(ch for ch in self.alphabet.keys())
 
     def encode(self, msg):
         result = ''
-        for ch in msg[:]:
+        for ch in msg[:].upper():
             # check if we know about this character
             if ch in self.allowed_characters:
+                print('## ', ch, self.alphabet[ch.upper()])
                 result += self.alphabet[ch.upper()] + ' '
             # skip unknown characters
         return result
 
-    def decode(self, msg, pos=0):
-        if pos < len(msg):
-            morse_letter = ''
-            for key, ch in enumerate(msg[pos:]):
-                if ch == ' ':
-                    pos = key + pos + 1
-                    letter = self.inveresed_alphabet[morse_letter]
-                    return letter + self.decode(msg, pos)
-                else:
-                    morse_letter += ch
-        else:
-            return ''
+    def decode(self, msg):
+        result = ''
+        words = msg.split('/')
+        for word in words:
+            print("# decoding ", word)
+            morse_letters = word.split()
+            letter = ''.join([self.inveresed_alphabet[l] for l in morse_letters])
+            result += ' ' + letter
+        return result
+
+
